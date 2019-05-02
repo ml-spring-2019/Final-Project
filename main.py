ad72ef2c799b318dfc5be5155c78680e693ecb1c
JohnK0 <johnskim97@gmail.com>
2019-05-02 10:14:27 -0700

    Jeremy and I added fedine_model_architecture, compile_fit_model, and evaluate_model functions

--- main.py
+++ main.py
@@ -15,21 +15,70 @@ from keras.utils import np_utils
 from matplotlib import pyplot as plt
 # from sklearn.preprocessing import StandardScaler
 
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+SCALE_WIDTH = 400
+SCALE_HEIGHT = 400
+NUM_CLASSES = 196
+
 def main(argv, argc):
     if argc != 2:
         print("Usage: python main.py <train rate>")
         exit(1)
 
     x_train, y_train, x_test, y_test = file_IO(argv)
-
-    # once x_train, y_train, etc. are images, reshape should work
-    pdb.set_trace()
-
+    model = define_model_architecture(x_train, y_train, x_test, y_test)
+    model = compile_fit_model(model, x_train, y_train, x_test, y_test)
+    evaluate_model(model, x_test, y_test)
     # Reshape input data
     print(x_train.shape)
     pdb.set_trace()
 
     return 0
+
+# ------------------------------------------------------------------------------
+# Define model architecture
+def define_model_architecture(x_train, y_train, x_test, y_test):
+    global NUM_CLASSES
+    model = Sequential()
+    model.add(Convolution2D(32, (3, 3), activation='relu', input_shape = (SCALE_WIDTH, SCALE_HEIGHT, 3)))
+    model.add(Convolution2D(32, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (2,2)))
+    model.add(Dropout(0.25))
+
+    pdb.set_trace()
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    pdb.set_trace()
+    return model
+
+# ------------------------------------------------------------------------------
+# Compile and fit the model
+# nb_epoch ~ number of times gone through the training set
+def compile_fit_model(model, x_train, y_train):
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    model.fit(x_train, y_train, batch_size=32, nb_epoch=100, verbose=1)
+    pdb.set_trace()
+
+    # this is our score after running model.fit
+    #hm, forgot what that means | me too... LOOL
+    # i'll try running with the sample code given, had to fix a few errors, runnning now
+    # the accuracy is 10%. Running with nb_epoch = 100  | ah ok, oh i see it says on the side when it's calculating
+    # Here's what i got for the sample one from the site: [0.02759441680457744, 0.9923]
+    # it seems like the right number is the
+    return model
+
+def evaluate_model(model, x_test, y_test):
+    score, accuracy = model.evaluate(x_test, y_test, verbose=0)
+    print("score: " + str(score))
+    print("accuracy: " + str(accuracy))
 
 # ------------------------------------------------------------------------------
 # read files
@@ -51,8 +100,7 @@ def rescale(img, width, height):
 # ------------------------------------------------------------------------------
 # preprocess input and output data
 def preprocess_data(df, train_rate):
-    SCALE_WIDTH = 400
-    SCALE_HEIGHT = 400
+    global SCALE_WIDTH, SCALE_HEIGHT, NUM_CLASSES
     features = []
     #    df = df.sample(frac=1)
     for index, row in df.iterrows():
@@ -80,12 +128,12 @@ def preprocess_data(df, train_rate):
 
     # x_train = x_train.reshape(x_train.shape[0], SCALE_WIDTH, SCALE_HEIGHT, 3)
     # x_test = x_test.reshape(x_test.shape[0], SCALE_WIDTH, SCALE_HEIGHT, 3)
-    y_train = np_utils.to_categorical(y_train,196)
-    pdb.set_trace()
+    y_train = np_utils.to_categorical(y_train,NUM_CLASSES)
+    y_test = np_utils.to_categorical(y_test,NUM_CLASSES)
 
     return x_train, y_train, x_test, y_test
 
-# ------------------------------------------------------------------------------
+# ---------------------------------------------------------i'm just getting set up since i just woke up haha---------------------
 # returns dataFrame of (train/test) data
 def parse_annos_file(cars_annos, train):
     mult_photos_info = []
